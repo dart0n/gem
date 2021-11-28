9c216434574ab292157b239dc7cc530cca150ca5
dart0n <dart0n@m.com>
2021-11-28 11:00:46 +0200

    Add game

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -4,8 +4,25 @@ require_relative 'codebreaker/version'
 
 module Codebreaker
   class Game
-    def self.start
-      'game started'
+    DIFFICULTIES = {
+      easy: { attempts: 15, hints: 2 },
+      medium: { attempts: 10, hints: 1 },
+      hell: { attempts: 5, hints: 1 }
+    }.freeze
+
+    attr_reader :secret_code, :attempts, :hints
+
+    def initialize(name, difficulty)
+      @username = name
+      @attempts = DIFFICULTIES[difficulty.to_sym][:attempts]
+      @hints = DIFFICULTIES[difficulty.to_sym][:hints]
+      @secret_code = generate_code
+    end
+
+    private
+
+    def generate_code
+      Array.new(4).map { rand(1..6) }.join
     end
   end
 end
